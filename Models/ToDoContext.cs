12d495fafc9a5fb820717270737fe7e8c4bcff93
Maksood Mohiuddin <maksood@gmail.com>
2020-08-17 19:11:10 -0400

    dotnet web api project scaffolded

---
+++ Models/ToDoContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace magnolia_cloudfoundry_webapinetcore.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
